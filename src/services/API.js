adf7d1727bd1b166c8223429e9724ce57615589a
Noah Korner <noahskorner@gmail.com>
2020-11-25 12:52:21 -0600

    Updated API calls

--- src/services/API.js
+++ src/services/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Vue from "vue";
 
 const apiClient = axios.create({
-  baseURL: "https://npdelivered.herokuapp.com",
+  baseURL: "https://npdelivered-backend.herokuapp.com",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -40,7 +40,7 @@ export default {
       config.headers["Authorization"] = `Token ${token}`;
     }
 
-    return axios.post("https://npdelivered.herokuapp.com/store/auth/logout/", null, config);
+    return axios.post("https://npdelivered-backend.herokuapp.com/store/auth/logout/", null, config);
   },
   getAffiliates() {
     return apiClient.get("/store/get-affiliates/");
