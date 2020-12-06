e8e70039b165c504710d74b5bd7c5f2951b0b9c1
Noah Korner <noahskorner@gmail.com>
2020-12-06 11:58:24 -0700

    Configured server for history mode

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const history = require('connect-history-api-fallback');
 
 const app = express()
 
 // serves files from our dist directory which now contains our index.html file
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// creates history mode for vue
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}));
 
 const port = process.env.PORT || 8080
 app.listen(port)
