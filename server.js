91170774253b05b86b692d40936fd45d0305b80a
Noah Korner <noahskorner@gmail.com>
2020-12-06 11:55:42 -0700

    Reverted back server

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-const express = require('express');
-const path = require('path');
-const history = require('connect-history-api-fallback');
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+// serves files from our dist directory which now contains our index.html file
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
+const port = process.env.PORT || 8080
+app.listen(port)
 
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
-
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+console.log('Listening on port: ' + port)
